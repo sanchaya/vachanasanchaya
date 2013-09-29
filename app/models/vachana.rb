1c27054b3c8ca27f2f1ddb816799a7b3c00da655
devudilip <devu.dilip@gmail.com>
2013-09-30 00:06:59 +0530

    wordlist search counting and if not there adding words comitted

--- app/models/vachana.rb
+++ app/models/vachana.rb
@@ -4,30 +4,50 @@ class Vachana < ActiveRecord::Base
 
 
   def self.search_vachana_pada(pada,type,author)
-  # vachanas=	where("vachana like ?", "%#{pada}%" )
-  if type && type == "like_search"
-    vachanas= where("vachana like ?", "%#{pada}%" )
-  else
-  # vachanas= where("vachana RLIKE ?","[[:<:]]#{pada}[[:>:]]" ) # exact search works with mysql
-  vachanas= where("vachana like ?", "%#{pada} %" )
-end
-unless author.blank?
-  vachanas = vachanas.where("vachanakaara_id = ?", "#{author}" )
-end
-@results = {}
-vachanas.each do |vachana|
- words = vachana.vachana.split
- count = 0
- words.each do |word|
-  count += 1 if word.upcase.include?(pada.upcase)
-end
-@results[vachana] = count
-end
-return @results
-end
+    # vachanas=	where("vachana like ?", "%#{pada}%" )
+    if type && type == "like_search"
+      vachanas= where("vachana like ?", "%#{pada}%" )
+    else
+      # vachanas= where("vachana RLIKE ?","[[:<:]]#{pada}[[:>:]]" ) # exact search works with mysql
+      vachanas= where("vachana like ?", "%#{pada} %" )
+    end
+
+    unless author.blank?
+      vachanas = vachanas.where("vachanakaara_id = ?", "#{author}" )
+    end
+    add_search_count(pada, type)
+    @results = {}
+    vachanas.each do |vachana|
+      words = vachana.vachana.split
+      count = 0
+      words.each do |word|
+        count += 1 if word.upcase.include?(pada.upcase)
+      end
+      @results[vachana] = count
+    end
+    return @results
+  end
 
 
+  def self.add_search_count(pada,type )
+    @search_pada = WordList.find_by_name(pada)
+    if @search_pada
+      if type && type == "like_search"
+        @search_pada.update_attribute('like_search_count', @search_pada.like_search_count + 1)
+      else
+        @search_pada.update_attribute('exact_search_count', @search_pada.exact_search_count + 1)
+      end
+    else
+      @new_search = WordList.new(name: pada)
+      if type && type == "like_search"
+       @new_search.like_search_count = 1
+     else
+      @new_search.exact_search_count = 1
+    end
+    @new_search.save
+  end
 
+end
 
 
 
