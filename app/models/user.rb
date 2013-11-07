7d956f177dd78d17f7b01beffa772f21fd5c06b4
devudilip <devu.dilip@gmail.com>
2013-11-07 13:40:19 +0530

    admin permission added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,27 @@
 class User < ActiveRecord::Base
+
+  before_create :default_user_role
+
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+ def is_admin?
+    user_role == "Admin"
+  end
+
+  private
+
+  def default_user_role
+    self.user_role = "Student"
+  end
+
+
 end
