530794cd611e5948a4275ec895ad39cf606c6e36
devudilip <devu.dilip@gmail.com>
2013-11-13 16:54:43 +0530

    onclick reload page with vachanakaara clicked fix

--- app/helpers/vachanas_helper.rb
+++ app/helpers/vachanas_helper.rb
@@ -1,10 +1,12 @@
 module VachanasHelper
 
 	def get_height_for_graph
-	if @vachanakaaras.count > 5
-		@vachanakaaras.count * 40
-else
-@vachanakaaras.count * 100
-end
+		if @vachanakaaras.count > 10
+			@vachanakaaras.count * 50
+		elsif @vachanakaaras.count < 5
+		    @vachanakaaras.count * 200
+		else
+			@vachanakaaras.count * 100
+		end
 	end
 end
