28b6cd11e76621f158fc1e2a8b4070545a117bc2
devudilip <devu.dilip@gmail.com>
2013-12-15 17:47:26 +0530

    added researches resources for samshodane concordance for keywords and listing according to alphabets

---
+++ app/controllers/researches_controller.rb
@@ -0,0 +1,12 @@
+class ResearchesController < ApplicationController
+	def index
+		if params[:start_letter]
+  		@keywords = KeyWord.start_letter(params[:start_letter])
+  	end
+
+  	respond_to do |format|
+  		format.html
+  format.js # actually means: if the client ask for js -> return file.js
+end
+	end
+end
