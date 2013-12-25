34ba5917bf5d5310bf86876451405322f9ed56b6
devudilip <devu.dilip@gmail.com>
2013-12-26 00:24:07 +0530

    concord keyword: show first letter result when nothing is selected

--- app/controllers/researches_controller.rb
+++ app/controllers/researches_controller.rb
@@ -2,6 +2,9 @@ class ResearchesController < ApplicationController
 	def index
 		if params[:start_letter]
   		@keywords = KeyWord.start_letter(params[:start_letter])
+  	else
+  		params[:start_letter] = "ಅ"
+  		@keywords = KeyWord.start_letter(params[:start_letter])
   	end
 
   	respond_to do |format|
