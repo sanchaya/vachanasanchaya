d8e68322cd513e2a73d3e6ed6b1d59c8f6fa7955
devudilip <devu.dilip@gmail.com>
2014-01-08 00:18:45 +0530

    keywords pagination on read more ajax

--- app/controllers/researches_controller.rb
+++ app/controllers/researches_controller.rb
@@ -1,7 +1,7 @@
 class ResearchesController < ApplicationController
 	def index
 		params[:start_letter] = params[:start_letter] ? params[:start_letter] : "ಅ"
-  		@keywords = KeyWord.start_letter(params[:start_letter])
+  		@keywords = KeyWord.start_letter(params[:start_letter]).order("count DESC").paginate(:page => params[:page])
   	respond_to do |format|
   		format.html
   format.js # actually means: if the client ask for js -> return file.js
