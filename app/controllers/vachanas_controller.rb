29ac185051d8ba65eceeeae8cbd87ebd4ccd963f
devudilip <devu.dilip@gmail.com>
2014-01-09 23:25:12 +0530

    layout changed for vachanas concord ajax load more added

--- app/controllers/vachanas_controller.rb
+++ app/controllers/vachanas_controller.rb
@@ -104,7 +104,7 @@ class VachanasController < ApplicationController
 
   def vachana_concord
    params[:start_letter] = params[:start_letter] ? params[:start_letter] : "ಅ"
-      @vachanas= Vachana.start_letter(params[:start_letter])
+      @vachanas= Vachana.start_letter(params[:start_letter]).paginate(:page => params[:page], :per_page => 25)
 
     respond_to do |format|
       format.html
