b1592231301fadcdf7864947905a75519666d89c
devudilip <devu.dilip@gmail.com>
2014-01-22 23:47:35 +0530

    updated google analytics to shiv sir account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ KannadaVachana::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  GA.tracker = "UA-47315168-1"
+  GA.tracker = "UA-3727897-19"
 end
