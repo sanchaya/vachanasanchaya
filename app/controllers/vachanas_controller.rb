de119ead2e2fc285bb93d3c51e2ecf16f8875be4
devudilip <devu.dilip@gmail.com>
2014-01-23 01:01:12 +0530

    vachana concord pagination limit to 15

--- app/controllers/vachanas_controller.rb
+++ app/controllers/vachanas_controller.rb
@@ -103,7 +103,7 @@ class VachanasController < ApplicationController
 
   def vachana_concord
    params[:start_letter] = params[:start_letter] ? params[:start_letter] : "ಅ"
-      @vachanas= Vachana.start_letter(params[:start_letter]).paginate(:page => params[:page], :per_page => 25)
+      @vachanas= Vachana.start_letter(params[:start_letter]).paginate(:page => params[:page], :per_page => 15)
 
     respond_to do |format|
       format.html
