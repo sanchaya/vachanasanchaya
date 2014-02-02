2aea358f13951a3f749ff1dc80f7c37eedb75070
devudilip <devu.dilip@gmail.com>
2014-02-02 16:55:26 +0530

    databse.yml conflict file deleted search box auto fill raw deleted copy clip board gem added and added in js

--- config/initializers/throttles.rb
+++ config/initializers/throttles.rb
@@ -1,13 +1,13 @@
 # Throttle requests to 5 requests per second per ip
-Rack::Attack.throttle('req/ip', :limit => 60, :period => 1.minute) do |req|
-  # If the return value is truthy, the cache key for the return value
-  # is incremented and compared with the limit. In this case:
-  #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
-  #
-  # If falsy, the cache key is neither incremented nor checked.
+# Rack::Attack.throttle('req/ip', :limit => 60, :period => 1.minute) do |req|
+#   # If the return value is truthy, the cache key for the return value
+#   # is incremented and compared with the limit. In this case:
+#   #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
+#   #
+#   # If falsy, the cache key is neither incremented nor checked.
 
-  req.ip
-end
+#   req.ip
+# end
 
 
 Rack::Attack.throttled_response = lambda do |env|
