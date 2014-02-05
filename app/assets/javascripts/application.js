4da6814375422f482730a78029daadba2f464edd
devudilip <devu.dilip@gmail.com>
2014-02-06 00:53:59 +0530

    jquery validation added gem file validation gem removed jquery validation for main search implemented

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require jquery.validate
 //= require highcharts
 //= require exporting
 //= require rangy-core
@@ -31,16 +32,52 @@ $( document ).ready( function () {
 	$( '.search-vachanakaara-name' ).ime();
 
 
+	$("#main_search").validate({
+		rules:{
+
+			"vachana":
+			{
+				required: true,
+				minlength: 2,
+				maxlength: 20
+			}
+			
+		},
+		messages:{
+			"vachana":
+			{
+				required: "ದಯವಿಟ್ಟು ಪದವನ್ನು ಬೆರಳಚ್ಚು ಮಾಡಿ",
+				minlength: "ಕನಿಷ್ಟ 2 ಅಕ್ಷರವನ್ನು ಬೆರಳಚ್ಚು ಮಾಡಿ",
+				maxlength: "User name should not exceed 20 letters"
+			}
+		},
+    // errorElement: "div",
+    //     wrapper: "div",  // a wrapper around the error message
+    //     errorPlacement: function(error, element) {
+    //       offset = element.offset();
+    //       error.insertBefore(element)
+    //         error.addClass('message');  // add a class to the wrapper
+    //         error.css('position', 'absolute');
+    //         error.css('left', offset.left + element.outerWidth());
+    //         error.css('top', offset.top);
+    //       }
+    errorPlacement: function (error, element) {
+    	alert(error.text());
+    }
+
+
+});
+
 } );
 
 
 
-function validateSearchForm()
-{
-	var x=document.forms["search_vachana"]["vachana"].value;
-	if (x==null || x=="")
-	{
-		alert("ದಯವಿಟ್ಟು ನಿಮಗೆ ಬೇಕಾದ ಪದವನ್ನು ಇಲ್ಲಿ ಬೆರಳಚ್ಚು ಮಾಡಿ");
-		return false;
-	}
-}
+// function validateSearchForm()
+// {
+// 	var x=document.forms["search_vachana"]["vachana"].value;
+// 	if (x==null || x=="")
+// 	{
+// 		alert("ದಯವಿಟ್ಟು ನಿಮಗೆ ಬೇಕಾದ ಪದವನ್ನು ಇಲ್ಲಿ ಬೆರಳಚ್ಚು ಮಾಡಿ");
+// 		return false;
+// 	}
+// }
