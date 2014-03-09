eff0d6bb43761b8cfda72bbd3967d77b66c75d5d
devudilip <devu.dilip@gmail.com>
2014-03-10 00:36:57 +0530

    admin panel users link typos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # before_filter :set_local_language
+  before_filter :set_local_language
 
   def set_local_language
     I18n.locale = 'kn'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,30 @@
 class UsersController < ApplicationController
+ before_filter :authenticate_user_role! 
+ def index
+  @users = User.all
+end
 
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
+def show
+  @user = User.find(params[:id])
+end
 
 
-  def new
-    @user = User.new
-  end
+def new
+  @user = User.new
+end
 
-  def create
-     @user = User.new(params[:user])
-     if @user.save
-      flash[:notice] = "User created successfully"
-      redirect_to @user
-     else
-      render "new"
-     end
-  end
+def create
+ @user = User.new(params[:user])
+ if @user.save
+  flash[:notice] = "User created successfully"
+  redirect_to @user
+else
+  render "new"
+end
+end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+def edit
+  @user = User.find(params[:id])
+end
 
 end
