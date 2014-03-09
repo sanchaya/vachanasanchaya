1f6a0ae6991e2198df613608c08f5081d5163425
devudilip <devu.dilip@gmail.com>
2014-03-10 01:48:08 +0530

    users name added addded index to uservachanakaara

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140309173328) do
+ActiveRecord::Schema.define(:version => 20140309201424) do
 
   create_table "concords", :force => true do |t|
     t.string   "name"
@@ -89,6 +89,9 @@ ActiveRecord::Schema.define(:version => 20140309173328) do
     t.datetime "updated_at",      :null => false
   end
 
+  add_index "user_vachanakaaras", ["user_id"], :name => "index_user_vachanakaaras_on_user_id"
+  add_index "user_vachanakaaras", ["vachanakaara_id"], :name => "index_user_vachanakaaras_on_vachanakaara_id"
+
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
@@ -104,6 +107,7 @@ ActiveRecord::Schema.define(:version => 20140309173328) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.integer  "role_id"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
