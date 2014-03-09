66726f2a656588b1cf695ecbf2fcc1524db79390
devudilip <devu.dilip@gmail.com>
2014-03-10 02:41:28 +0530

    cancan added ability load authorize for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_local_language
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
   def set_local_language
     I18n.locale = 'kn'
   end
 
   def authenticate_user_role!
-    if current_user && current_user.is_admin?
+    if current_user && (current_user.is_admin? or current_user.role.name == "Admin")
       true
     else
       flash[:error] = "Sorry you dont have permission"
@@ -15,5 +19,5 @@ class ApplicationController < ActionController::Base
     end
   end
 
- 
+
 end
