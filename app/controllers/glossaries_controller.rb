852a2d267fb704db5de41f92988a953f371cb45b
devudilip <devu.dilip@gmail.com>
2014-03-31 01:12:46 +0530

    cancanc

--- app/controllers/glossaries_controller.rb
+++ app/controllers/glossaries_controller.rb
@@ -3,11 +3,11 @@ class GlossariesController < ApplicationController
   def index
     @words = Glossary.order("word").limit(100)
   end
-  def destroy
-    @glossary = Glossary.find(params[:id])
-    @glossary.destroy
-    redirect_to glossaries_path
-  end
+  # def destroy
+  #   @glossary = Glossary.find(params[:id])
+  #   @glossary.destroy
+  #   redirect_to glossaries_path
+  # end
 
   def search 
     @word = params[:word].to_s
