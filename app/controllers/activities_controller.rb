f36b72a838706f94937f086b19a9e62848d373cd
devudilip <devu.dilip@gmail.com>
2014-04-06 23:26:37 +0530

    public activity updated views activity params and current user added

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,8 @@
 class ActivitiesController < ApplicationController
-  def index
-    @activities = PublicActivity::Activity.order("created_at desc")
-  end
+ before_filter :authenticate_user_role!
+ # load_and_authorize_resource
+ 
+ def index
+  @activities = PublicActivity::Activity.order("created_at desc").limit(20)
+end
 end
