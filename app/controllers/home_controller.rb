2f40af9b1e7bc83db42737acdd840f66ee08adf2
devudilip <devu.dilip@gmail.com>
2014-07-19 02:26:15 +0530

    Issue #1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,24 @@ class HomeController < ApplicationController
   before_filter :authenticate_user_role! , only: [:admin_panel]
   def index
     @rand_vachana = DailyVachana.last 
-    unless @rand_vachana and @rand_vachana.created_at.to_date == Date.today
-      rand = Vachana.pluck(:id).sample
+    begin
+      unless @rand_vachana and @rand_vachana.created_at.to_date == Date.today
+       rand = Vachana.pluck(:id).sample
+       while Vachana.find(rand).vachana.blank?
+        rand = Vachana.pluck(:id).sample
+      end
       @rand_vachana = DailyVachana.create vachana_id: rand
     end
-    @vachana = @rand_vachana.vachana
+  rescue => e
+    p "ERRRORRRRRR"
+    puts e
   end
+  @vachana = @rand_vachana.vachana
+end
 
-  def admin_panel
-  end
+def admin_panel
+end
+
+private
 
 end
