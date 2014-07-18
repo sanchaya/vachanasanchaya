88bac26a06ff898e90e59925bbe4092dc205d927
devudilip <devu.dilip@gmail.com>
2014-07-19 01:35:35 +0530

    rack attack updated

--- config/initializers/throttles.rb
+++ config/initializers/throttles.rb
@@ -1,25 +1,70 @@
-# Throttle requests to 5 requests per second per ip
-Rack::Attack.throttle('req/ip', :limit => 30, :period => 1.minute) do |req|
-  # If the return value is truthy, the cache key for the return value
-  # is incremented and compared with the limit. In this case:
-  #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
+  ### Configure Cache ###
+
+  # If you don't want to use Rails.cache (Rack::Attack's default), then
+  # configure it here.
   #
-  # If falsy, the cache key is neither incremented nor checked.
+  # Note: The store is only used for throttling (not blacklisting and
+  # whitelisting). It must implement .increment and .write like
+  # ActiveSupport::Cache::Store
 
-  req.ip
-end
+  # Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new 
 
+  ### Throttle Spammy Clients ###
 
-Rack::Attack.throttled_response = lambda do |env|
-  # name and other data about the matched throttle
-  # body = [
-  #   env['rack.attack.matched'],
-  #   env['rack.attack.match_type'],
-  #   env['rack.attack.match_data']
-  #   ].inspect
+  # If any single client IP is making tons of requests, then they're probably
+  # malicious or a poorly-configured scraper. Either way, they don't deserve
+  # to hog all of the app server's CPU. Cut them off!
 
-  # Using 503 because it may make attacker think that they have successfully
-  # DOSed the site. Rack::Attack returns 429 for throttling by default
-  # [ 503, {}, [body]]
-  [ 503, {}, ["ವಚನ ಸಂಚಯವನ್ನು ನಿಧಾನವಾಗಿ ಓದಿ... ದಯವಿಟ್ಟು ಸ್ವಲ್ಪ ಸಮಯದ ನಂತರ ಪುನಃ ಪ್ರಯತ್ನಿಸಿ "]]
-end
+  # Throttle all requests by IP (60rpm)
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
+  Rack::Attack.throttle('req/ip', :limit => 300, :period => 5.minutes) do |req|
+    req.ip
+  end
+
+  ### Prevent Brute-Force Login Attacks ###
+
+  # The most common brute-force login attack is a brute-force password attack
+  # where an attacker simply tries a large number of emails and passwords to
+  # see if any credentials match.
+  #
+  # Another common method of attack is to use a swarm of computers with
+  # different IPs to try brute-forcing a password for a specific account.
+
+  # Throttle POST requests to /login by IP address
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:logins/ip:#{req.ip}"
+  Rack::Attack.throttle('logins/ip', :limit => 5, :period => 20.seconds) do |req|
+    if req.path == '/login' && req.post?
+      req.ip
+    end
+  end
+
+  # Throttle POST requests to /login by email param
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:logins/email:#{req.email}"
+  #
+  # Note: This creates a problem where a malicious user could intentionally
+  # throttle logins for another user and force their login requests to be
+  # denied, but that's not very common and shouldn't happen to you. (Knock on
+  # wood!)
+  Rack::Attack.throttle("logins/email", :limit => 5, :period => 20.seconds) do |req|
+    if req.path == '/login' && req.post?
+      # return the email if present, nil otherwise
+      req.params['email'].presence
+    end
+  end
+
+  ### Custom Throttle Response ###
+
+  # By default, Rack::Attack returns an HTTP 429 for throttled responses,
+  # which is just fine.
+  #
+  # If you want to return 503 so that the attacker might be fooled into
+  # believing that they've successfully broken your app (or you just want to
+  # customize the response), then uncomment these lines.
+  # self.throttled_response = lambda do |env|
+  #  [ 503,  # status
+  #    {},   # headers
+  #    ['']] # body
+  # end
