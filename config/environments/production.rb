8f9bbfddb6ff14f9c14103f19717268e45f2e6ee
devudilip <devu.dilip@gmail.com>
2014-07-20 00:58:44 +0530

    enabling memcache using dalli

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,6 @@ KannadaVachana::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
@@ -61,6 +60,8 @@ config.i18n.fallbacks = true
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+# Enabling mem_cache_store
+config.cache_store = :dalli_store
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
